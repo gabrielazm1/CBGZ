f5655ce942eb11ce931f9633e68ab13373797b6c
camilapborba <camilapb6@al.insper.edu.com.br>
2026-04-08 18:33:03 -0300

    questão 2

--- funcoes.py
+++ funcoes.py
@@ -7,3 +7,16 @@ def rolar_dados(n):
         lista.append(x)
         i = i + 1
     return lista
+
+def guardar_dado (dados_rolados,dados_no_estoque,dado_para_guardar):
+    dados_no_estoque.append(dados_rolados[dado_para_guardar])
+    nova_lista = []
+    i = 0
+    while i < len(dados_rolados):
+        if i != dado_para_guardar:
+            nova_lista.append(dados_rolados[i])
+        i = i + 1
+
+    return [nova_lista, dados_no_estoque]
+
+            
